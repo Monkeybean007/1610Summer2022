073a70c0ff6ea745996347f801e05a49d681c06c
Seth Corbran <monkeybean1464@gmail.com>
2022-07-16 11:34:04 -0400

    Added Random UFO Spawning, Added Random UFO location Spawning, Edited Prefabs

--- Unity/UFO Defense Force Game/Assets/Scripts/EnemySpawnManager.cs
+++ Unity/UFO Defense Force Game/Assets/Scripts/EnemySpawnManager.cs
@@ -7,13 +7,23 @@ using UnityEngine;
 public class EnemySpawnManager : MonoBehaviour
 {
     public GameObject[] ufoPrefabs; //Array to store UFO ships
-    public int ufoIndex;
+    private float spawnRangeX = 20f;
+
+    private float spawnPosZ = 20f;
+
+    
 
 
     // Update is called once per frame
     void Update()
+
     {
-        if(Input.GetKeyDown(KeyCode.S))
-        Instantiate(ufoPrefabs[ufoIndex],new Vector3(0,0,15),ufoPrefabs[ufoIndex],transform.rotation);
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ); // Spawn UfO ar random location
+        int ufoIndex = Random.Range(0,ufoPrefabs.Length); //Pick a random UFO fom the array
+        Instantiate(ufoPrefabs[ufoIndex],spawnPos,ufoPrefabs[ufoIndex].transform.rotation); //Spawns a indexed UFO from the array
+        }
     }
 }
--- Unity/UFO Defense Force Game/Assets/Scripts/MoveForward.cs
+++ Unity/UFO Defense Force Game/Assets/Scripts/MoveForward.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveForward : MonoBehaviour
-{   public float speed = 50.0f;
+{   public float speed = 20.0f;
     // Update is called once per frame
     void Update()
     {
