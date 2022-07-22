f8462590959822394802d536752ae15175393100
Seth Corbran <monkeybean1464@gmail.com>
2022-07-22 11:32:24 -0400

    Worked on Project

--- Unity/UFO Defense Force Game/Assets/Scripts/CoinController.cs
+++ Unity/UFO Defense Force Game/Assets/Scripts/CoinController.cs
@@ -6,6 +6,47 @@ using TMPro;
 
 public class CoinController : MonoBehaviour
 {
+    public GameObject[] coinPrefabs; //Array to store coins
+    private float spawnRangeX = 20f;
+
+    private float spawnPosZ = 20f;
+
+    private float startDelay = 8f;
+
+    private float spawnInterval = 5f;
+
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomCoin", startDelay, spawnInterval);
+    }
+    
+
+
+    // Update is called once per frame
+    void Update()
+
+    {
+
+        
+    }
+    void SpawnRandomCoin()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ); // Spawn Coin at random location
+        int coinIndex = Random.Range(0,coinPrefabs.Length); //Pick a random Object fom the array
+        Instantiate(coinPrefabs[coinIndex],spawnPos,coinPrefabs[coinIndex].transform.rotation); //Spawns a indexed Objectfrom the array
+
+    }
+}
+
+
+
+
+
+
+
+
+/*{
 private float coin = 0;
 
 public TextMeshProUGUI textCoins;
@@ -22,4 +63,4 @@ private void OnTriggerEnter(Collider collision)
 }
 
 
-}
+}*/
