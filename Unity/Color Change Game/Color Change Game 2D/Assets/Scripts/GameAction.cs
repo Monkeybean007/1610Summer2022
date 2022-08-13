93b8274584dcabc8a055a7aff382486048b39fd8
Seth Corbran <monkeybean1464@gmail.com>
2022-08-13 01:07:02 -0400

    Created and Added GameAction and GameActionHandler Script

---
+++ Unity/Color Change Game/Color Change Game 2D/Assets/Scripts/GameAction.cs
@@ -0,0 +1,16 @@
+using UnityEngine.Events;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class GameAction : ScriptableObject
+{
+ public UnityAction raise;
+
+public void RaiseAction ()
+{
+    raise?.Invoke();
+
+}
+
+}
